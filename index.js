47b1d997dfdfd5c34d05b1e96b99a8ed5d0bfafd
viet-2304 <vietphama7@gmail.com>
2022-02-27 18:00:31 +0700

    UI and Add element

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 
+function setCount() {
+    var number =  document.getElementsByClassName("count");
+   number.innerHTML = document.getElementsByClassName('check').length;
+    console.log(document.getElementsByClassName("count").innerHTML);
+
+}
+
 function addItem(event) {
+    var todoList = document.querySelector("ul")
     if (event.keyCode == 13) {
-        var  newMenu = document.getElementById("myMenu")
-       var valueInput = document.getElementById("input").value
-        var val = document.createElement('menuitem')
-        newMenu.appendChild(val)
-        console.log(newMenu)
+        var valueInput = document.getElementById("input").value
+        var element = document.createElement('li')
+        element.innerHTML = `<div class="view">
+        <input class="check" type="checkbox">
+        <label>${valueInput}</label>
+        <button class="remove"></button>
+        </div>`
+        document.querySelector("input").value =''
+        todoList.appendChild(element)
+        setCount();
     }
 }
+
