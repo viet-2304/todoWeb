53d8b7cd7b1e3801c413db4bd3f2dcbfd355e8e5
viet-2304 <vietphama7@gmail.com>
2022-03-16 09:56:47 +0700

    saving data in localstorage

--- dist/index.js
+++ dist/index.js
@@ -2,6 +2,8 @@ var todoList = document.querySelector("ul");
 var itemLeft = document.getElementById("item-left");
 var clearAll = document.getElementById("clear-completed");
 var checkAll = document.getElementById("label-check-all");
+var listToDos = [];
+var dataInLocalStorage;
 todoList.addEventListener("click", function (event) {
     if (event.target.classList.contains("remove")) {
         event.target.parentElement.remove();
@@ -42,16 +44,19 @@ function checkItemLeft() {
     }
 }
 function addItem(event) {
-    var valueInput = document.getElementById("input").value;
+    var valueInput = document.getElementById("input")
+        .value;
     var todoList = document.querySelector("ul");
     var letterNumber = /^[ ]+$/;
     if (event.keyCode == 13 &&
         valueInput.match(letterNumber) == null &&
         valueInput.length != 0) {
+        var newItem = "<div class=\"view\">\n        <input class=\"check\" type=\"checkbox\" id=\"btnCheck\">\n        <label  id=\"text-label\" contenteditable=\"true\">".concat(valueInput, "</label>\n        <button class=\"remove\"></button>\n        </div>\n        <input id=\"edit\" type=\"text\" class=\"edit hidden\" value=''>");
         var element = document.createElement("li");
-        element.innerHTML = "<div class=\"view\">\n        <input class=\"check\" type=\"checkbox\" id=\"btnCheck\">\n        <label  id=\"text-label\" contenteditable=\"true\">".concat(valueInput, "</label>\n        <button class=\"remove\"></button>\n        </div>\n        <input id=\"edit\" type=\"text\" class=\"edit hidden\" value=''>");
+        element.innerHTML = newItem;
         document.querySelector("input").value = "";
         todoList.appendChild(element);
+        saveToDos({ item: valueInput, isComplete: "false" });
         setCount();
     }
 }
@@ -59,7 +64,9 @@ function removeItem(element) {
     element.remove();
     setCount();
 }
-document.querySelector(".clear-completed").addEventListener("click", function () {
+document
+    .querySelector(".clear-completed")
+    .addEventListener("click", function () {
     document
         .querySelectorAll('div input[type="checkbox"]:checked')
         .forEach(function (item) {
@@ -72,6 +79,7 @@ function findAll() {
     allItems.forEach(function (item) {
         item.classList.remove("hidden");
     });
+    changeSelectFilter("find-all");
 }
 function findActive() {
     var allItems = todoList.querySelectorAll("li");
@@ -80,6 +88,7 @@ function findActive() {
             ? item.classList.add("hidden")
             : item.classList.remove("hidden");
     });
+    changeSelectFilter("find-active");
 }
 function findComplete() {
     var allItems = todoList.querySelectorAll("li");
@@ -88,11 +97,93 @@ function findComplete() {
             ? item.classList.remove("hidden")
             : item.classList.add("hidden");
     });
+    changeSelectFilter("find-complete");
 }
-document.getElementById("label-check-all").addEventListener("click", function () {
+function changeSelectFilter(choose) {
+    var active = document.getElementById("find-active");
+    var complete = document.getElementById("find-complete");
+    var all = document.getElementById("find-all");
+    if (choose == "find-active") {
+        active.classList.add("selected");
+        complete.classList.remove("selected");
+        all.classList.remove("selected");
+    }
+    if (choose == "find-all") {
+        active.classList.remove("selected");
+        complete.classList.remove("selected");
+        all.classList.add("selected");
+    }
+    if (choose == "find-complete") {
+        active.classList.remove("selected");
+        complete.classList.add("selected");
+        all.classList.remove("selected");
+    }
+}
+document
+    .getElementById("label-check-all")
+    .addEventListener("click", function () {
     var allCheckbox = document.querySelectorAll("input[type=checkbox]");
     allCheckbox.forEach(function (element) {
         element.checked = true;
     });
     setCount();
 });
+var toDos = [
+    {
+        name: "",
+        isComplete: ""
+    },
+];
+function saveToDos(toDos) {
+    listToDos.push(toDos);
+    window.localStorage.setItem("toDos", JSON.stringify(listToDos));
+}
+function changeState(position, state) {
+    var count = 0;
+    console.log(listToDos.indexOf(position));
+}
+function getLocalStorage() {
+    return window.localStorage.getItem("toDos");
+}
+window.onload = function () {
+    var list = JSON.parse(window.localStorage.getItem("toDos"));
+    console.log(getLocalStorage());
+    dataInLocalStorage = getLocalStorage();
+    if (list != null) {
+        list.forEach(function (ele) {
+            var element = document.createElement("li");
+            var divView = document.createElement("div");
+            var inputCheck = document.createElement("input");
+            var lbText = document.createElement("label");
+            var btRemove = document.createElement("button");
+            divView.classList.add("view");
+            inputCheck.classList.add("check");
+            inputCheck.setAttribute("type", "checkbox");
+            inputCheck.setAttribute("id", "btnCheck");
+            inputCheck.setAttribute("onclick", "checkIsComplete()");
+            if (ele.isComplete == "true") {
+                inputCheck.checked = true;
+            }
+            lbText.setAttribute("id", "text-label");
+            lbText.setAttribute("contenteditable", "true");
+            lbText.textContent = ele.item;
+            btRemove.classList.add("remove");
+            divView.appendChild(inputCheck);
+            divView.appendChild(lbText);
+            divView.appendChild(btRemove);
+            element.appendChild(divView);
+            todoList.appendChild(element);
+        });
+        setCount();
+    }
+};
+window.onbeforeunload = function () {
+    var listData;
+    if (dataInLocalStorage == null) {
+        listData = window.localStorage.getItem("toDos");
+    }
+    else {
+        listData = dataInLocalStorage.concat(JSON.stringify(window.localStorage.getItem("toDos")));
+    }
+    window.localStorage.setItem("toDos", listData);
+};
--- src/index.ts
+++ src/index.ts
@@ -1,103 +1,211 @@
-const todoList: any = document.querySelector("ul");
-const itemLeft: any = document.getElementById("item-left");
-const clearAll: any = document.getElementById("clear-completed");
-const checkAll: any = document.getElementById("label-check-all");
-todoList.addEventListener("click", (event: MouseEvent) : void => {
-    if ((event.target as HTMLTextAreaElement).classList.contains("remove")) {
-        (event.target as HTMLTextAreaElement).parentElement.remove();
-        setCount();
-    }
-    if ((event.target as HTMLTextAreaElement).classList.contains("check")) {
-        setCount();
-    }
+const todoList: HTMLElement = document.querySelector("ul");
+const itemLeft: HTMLElement = document.getElementById("item-left");
+const clearAll: HTMLElement = document.getElementById("clear-completed");
+const checkAll: HTMLElement = document.getElementById("label-check-all");
+const listToDos = [];
+var dataInLocalStorage: string;
+todoList.addEventListener("click", (event: MouseEvent): void => {
+  if ((event.target as HTMLTextAreaElement).classList.contains("remove")) {
+    (event.target as HTMLTextAreaElement).parentElement.remove();
+    setCount();
+  }
+  if ((event.target as HTMLTextAreaElement).classList.contains("check")) {
+    setCount();
+  }
 });
-todoList.addEventListener("keypress", (event: KeyboardEvent) : void => {
-    if (event.keyCode === 13) {
-        event.preventDefault();
-    }
+todoList.addEventListener("keypress", (event: KeyboardEvent): void => {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+  }
 });
-function setCount() : void {
-    var checkNumber: any = document.querySelectorAll('div input[type="checkbox"]:checked').length;
-    var allCheckBox: any = document.getElementsByClassName("check").length;
-    itemLeft.innerHTML = (allCheckBox - checkNumber).toString();
-    if (checkNumber != 0) {
-        clearAll.classList.remove("hidden");
-    }
-    else {
-        clearAll.classList.add("hidden");
-    }
-    checkItemLeft();
+function setCount(): void {
+  var checkNumber: number = document.querySelectorAll(
+    'div input[type="checkbox"]:checked'
+  ).length;
+  var allCheckBox: number = document.getElementsByClassName("check").length;
+  itemLeft.innerHTML = (allCheckBox - checkNumber).toString();
+  if (checkNumber != 0) {
+    clearAll.classList.remove("hidden");
+  } else {
+    clearAll.classList.add("hidden");
+  }
+  checkItemLeft();
 }
-function checkItemLeft() : void{
-    var element: any = document.getElementById("footer");
-    var checkNumber: any = document.querySelectorAll('div input[type="checkbox"]:checked').length;
-    var itemLeftText: any = document.getElementById("item-left").innerHTML;
-    if (itemLeftText !== "0") {
-        element.classList.remove("hidden");
-        checkAll.classList.remove("hidden");
-    }
-    if (itemLeftText === "0" && checkNumber === 0) {
-        element.classList.add("hidden");
-        checkAll.classList.add("hidden");
-    }
+function checkItemLeft(): void {
+  var element: HTMLElement = document.getElementById("footer");
+  var checkNumber: number = document.querySelectorAll(
+    'div input[type="checkbox"]:checked'
+  ).length;
+  var itemLeftText: String = document.getElementById("item-left").innerHTML;
+  if (itemLeftText !== "0") {
+    element.classList.remove("hidden");
+    checkAll.classList.remove("hidden");
+  }
+  if (itemLeftText === "0" && checkNumber === 0) {
+    element.classList.add("hidden");
+    checkAll.classList.add("hidden");
+  }
 }
-function addItem(event: KeyboardEvent) : void {
-    var valueInput: String = (<HTMLInputElement>document.getElementById("input")).value;
-    var todoList: any = document.querySelector("ul");
-    var letterNumber = /^[ ]+$/;
-    if (event.keyCode == 13 &&
-        valueInput.match(letterNumber) == null &&
-        valueInput.length != 0) {
-        var element: any = document.createElement("li");
-        element.innerHTML = `<div class="view">
+function addItem(event: KeyboardEvent): void {
+  var valueInput: String = (<HTMLInputElement>document.getElementById("input"))
+    .value;
+  var todoList: HTMLElement = document.querySelector("ul");
+  var letterNumber = /^[ ]+$/;
+  if (
+    event.keyCode == 13 &&
+    valueInput.match(letterNumber) == null &&
+    valueInput.length != 0
+  ) {
+    var newItem: string = `<div class="view">
         <input class="check" type="checkbox" id="btnCheck">
         <label  id="text-label" contenteditable="true">${valueInput}</label>
         <button class="remove"></button>
         </div>
         <input id="edit" type="text" class="edit hidden" value=''>`;
-        document.querySelector("input").value = "";
-        todoList.appendChild(element);
-        setCount();
-    }
-}
-function removeItem(element: Element) : void {
-    element.remove();
+    var element: any = document.createElement("li");
+    element.innerHTML = newItem;
+    document.querySelector("input").value = "";
+    todoList.appendChild(element);
+    saveToDos({ item: valueInput, isComplete: "false" });
     setCount();
+  }
 }
-document.querySelector(".clear-completed").addEventListener("click", () : void => {
+function removeItem(element: Element): void {
+  element.remove();
+  setCount();
+}
+document
+  .querySelector(".clear-completed")
+  .addEventListener("click", (): void => {
     document
-        .querySelectorAll('div input[type="checkbox"]:checked')
-        .forEach((item) => {
+      .querySelectorAll('div input[type="checkbox"]:checked')
+      .forEach((item) => {
         removeItem(item.closest("li"));
-    });
+      });
     checkItemLeft();
-});
-function findAll() : void{
-    var allItems = todoList.querySelectorAll("li");
-    allItems.forEach((item) => {
-        item.classList.remove("hidden");
-    });
+  });
+function findAll(): void {
+  var allItems: NodeListOf<HTMLLIElement> = todoList.querySelectorAll("li");
+  allItems.forEach((item) => {
+    item.classList.remove("hidden");
+  });
+  changeSelectFilter("find-all");
 }
-function findActive() : void {
-    var allItems: any = todoList.querySelectorAll("li");
-    allItems.forEach((item) => {
-        item.querySelector(":checked")
-            ? item.classList.add("hidden")
-            : item.classList.remove("hidden");
-    });
+function findActive(): void {
+  var allItems: NodeListOf<HTMLLIElement> = todoList.querySelectorAll("li");
+  allItems.forEach((item) => {
+    item.querySelector(":checked")
+      ? item.classList.add("hidden")
+      : item.classList.remove("hidden");
+  });
+  changeSelectFilter("find-active");
 }
-function findComplete() : void {
-    var allItems: any = todoList.querySelectorAll("li");
-    allItems.forEach((item) => {
-        item.querySelector(":checked")
-            ? item.classList.remove("hidden")
-            : item.classList.add("hidden");
-    });
+function findComplete(): void {
+  var allItems: NodeListOf<HTMLLIElement> = todoList.querySelectorAll("li");
+  allItems.forEach((item) => {
+    item.querySelector(":checked")
+      ? item.classList.remove("hidden")
+      : item.classList.add("hidden");
+  });
+  changeSelectFilter("find-complete");
 }
-document.getElementById("label-check-all").addEventListener("click", () : void => {
-    var allCheckbox: any = document.querySelectorAll("input[type=checkbox]");
+function changeSelectFilter(choose: String): void {
+  var active: HTMLElement = document.getElementById("find-active");
+  var complete: HTMLElement = document.getElementById("find-complete");
+  var all: HTMLElement = document.getElementById("find-all");
+  if (choose == "find-active") {
+    active.classList.add("selected");
+    complete.classList.remove("selected");
+    all.classList.remove("selected");
+  }
+  if (choose == "find-all") {
+    active.classList.remove("selected");
+    complete.classList.remove("selected");
+    all.classList.add("selected");
+  }
+  if (choose == "find-complete") {
+    active.classList.remove("selected");
+    complete.classList.add("selected");
+    all.classList.remove("selected");
+  }
+}
+document
+  .getElementById("label-check-all")
+  .addEventListener("click", (): void => {
+    var allCheckbox: NodeListOf<Element> = document.querySelectorAll(
+      "input[type=checkbox]"
+    );
     allCheckbox.forEach((element) => {
-        (element as HTMLInputElement).checked = true;
+      (element as HTMLInputElement).checked = true;
     });
     setCount();
-});
+  });
+
+const toDos: object = [
+  {
+    name: "",
+    isComplete: "",
+  },
+];
+
+function saveToDos(toDos: object): void {
+  listToDos.push(toDos);
+  window.localStorage.setItem("toDos", JSON.stringify(listToDos));
+}
+
+function changeState(position: number, state: boolean): void {
+  var count: number = 0;
+  console.log(listToDos.indexOf(position));
+}
+
+function getLocalStorage(): string {
+  return window.localStorage.getItem("toDos");
+}
+
+window.onload = function () {
+  var list = JSON.parse(window.localStorage.getItem("toDos"));
+  console.log(getLocalStorage());
+  dataInLocalStorage = getLocalStorage();
+  if (list != null) {
+    list.forEach((ele) => {
+      var element: HTMLLIElement = document.createElement("li");
+      var divView: HTMLDivElement = document.createElement("div");
+      var inputCheck: HTMLInputElement = document.createElement("input");
+      var lbText: HTMLLabelElement = document.createElement("label");
+      var btRemove: HTMLButtonElement = document.createElement("button");
+
+      divView.classList.add("view");
+      inputCheck.classList.add("check");
+      inputCheck.setAttribute("type", "checkbox");
+      inputCheck.setAttribute("id", "btnCheck");
+      inputCheck.setAttribute("onclick", "checkIsComplete()");
+      if (ele.isComplete == "true") {
+        inputCheck.checked = true;
+      }
+      lbText.setAttribute("id", "text-label");
+      lbText.setAttribute("contenteditable", "true");
+      lbText.textContent = ele.item;
+      btRemove.classList.add("remove");
+
+      divView.appendChild(inputCheck);
+      divView.appendChild(lbText);
+      divView.appendChild(btRemove);
+      element.appendChild(divView);
+
+      todoList.appendChild(element);
+    });
+    setCount();
+  }
+};
+
+window.onbeforeunload = function () {
+  var listData;
+  if (dataInLocalStorage == null) {
+    listData = window.localStorage.getItem("toDos");
+  } else {
+    listData = dataInLocalStorage.concat(
+      JSON.stringify(window.localStorage.getItem("toDos"))
+    );
+  }
+  window.localStorage.setItem("toDos", listData);
+};
